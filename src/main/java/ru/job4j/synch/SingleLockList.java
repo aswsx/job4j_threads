cba9c0617e1d8dcd29872c87db0d28f092abe0c9
Tosh-PC <lextor78@gmail.com>
2022-02-14 23:41:57 +0600

    4. ThreadSafe динамический список

---
+++ src/main/java/ru/job4j/synch/SingleLockList.java
@@ -0,0 +1,44 @@
+package ru.job4j.synch;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Alex Gutorov
+ * @version 1.0
+ * @created 14/02/2022 - 22:40
+ */
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
+    private final List<T> list;
+
+    public SingleLockList(List<T> list) {
+        this.list = (List<T>) copy(list);
+    }
+
+    public SingleLockList() {
+        this.list = new ArrayList<>();
+    }
+
+    public synchronized void add(T value) {
+        list.add(value);
+    }
+
+    public synchronized T get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return copy(this.list).iterator();
+    }
+
+    private synchronized Iterable<T> copy(List<T> list) {
+        return new ArrayList<>(list);
+    }
+}
---
+++ src/test/java/ru/job4j/synch/SingleLockListTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.synch;
+
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Alex Gutorov
+ * @version 1.0
+ * @created 14/02/2022 - 22:42
+ */
+public class SingleLockListTest {
+
+    @Test
+    public void add() throws InterruptedException {
+        SingleLockList<Integer> list = new SingleLockList<>();
+        Thread first = new Thread(() -> list.add(1));
+        Thread second = new Thread(() -> list.add(2));
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Set<Integer> rsl = new TreeSet<>();
+        list.iterator().forEachRemaining(rsl::add);
+        assertThat(rsl, is(Set.of(1, 2)));
+    }
+}
