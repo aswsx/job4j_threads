5abef81212774a5f1070b5b69776a7c27d463b84
Tosh-PC <lextor78@gmail.com>
2022-03-06 17:33:03 +0600

    0. CAS - операции

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author Alex Gutorov
+ * @version 1.0
+ * @created 06/03/2022 - 17:05
+ */
+@ThreadSafe
+public class CASCount {
+
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int temp;
+        do {
+            temp = count.get();
+        } while (!count.compareAndSet(temp, temp++));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
+
