9be823e21edeb7078c89a4bb12a3dce3581aad38
Tosh-PC <lextor78@gmail.com>
2022-03-12 16:47:46 +0600

    3. ForkJoinPool

--- src/main/java/ru/job4j/pools/ParallelIndexFinder.java
+++ src/main/java/ru/job4j/pools/ParallelIndexFinder.java
@@ -8,41 +8,53 @@ import java.util.concurrent.RecursiveTask;
  * @version 1.0
  * @created 12/03/2022 - 12:43
  */
-public class ParallelIndexFinder extends RecursiveTask<Integer> {
-    private final Integer[] array;
-    private final Integer sample;
-    private final Integer from;
-    private final Integer to;
+public class ParallelIndexFinder<T> extends RecursiveTask<Integer> {
+    private transient T[] array;
+    private transient T sample;
+    private int from;
+    private int to;
 
-    public ParallelIndexFinder(Integer[] array, Integer sample, Integer from, Integer to) {
+    public ParallelIndexFinder(T[] array, T sample, int from, int to) {
         this.array = array;
         this.sample = sample;
         this.from = from;
         this.to = to;
     }
 
+    public ParallelIndexFinder(T[] array, T sample) {
+        this.array = array;
+        this.sample = sample;
+        this.from = 0;
+        this.to = array.length - 1;
+    }
+
+    public ParallelIndexFinder() {
+    }
+
+    public Integer findIndex(T[] array, T sample) {
+        return new ForkJoinPool()
+                .invoke(new ParallelIndexFinder<>(array, sample));
+    }
+
+    private int linearSearch() {
+        for (var i = from; i <= to; i++) {
+            if (array[i].equals(sample)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
     protected Integer compute() {
         if (from - to <= 10) {
-            for (var i = 0; i < array.length; i++) {
-                if (array[i].equals(sample)) {
-                    return i;
-                }
-            }
-            return -1;
+            return linearSearch();
         }
-
         var mid = (from + to) / 2;
-        var left = new ParallelIndexFinder(array, sample, from, mid);
-        var right = new ParallelIndexFinder(array, sample, mid + 1, to);
+        ParallelIndexFinder<T> left = new ParallelIndexFinder<>(array, sample, from, mid);
+        ParallelIndexFinder<T> right = new ParallelIndexFinder<>(array, sample, mid + 1, to);
         left.fork();
         right.fork();
         return Math.max(left.join(), right.join());
-    }
-
-    public static Integer findIndex(Integer[] array, Integer sample) {
-        var forkJoinPool = new ForkJoinPool();
-        return forkJoinPool
-                .invoke(new ParallelIndexFinder(array, sample, 0, array.length - 1));
     }
 }
--- src/test/java/ru/job4j/pools/ParallelIndexFinderTest.java
+++ src/test/java/ru/job4j/pools/ParallelIndexFinderTest.java
@@ -13,30 +13,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ParallelIndexFinderTest {
 
     @Test
-    public void whenLinearSearchNotFoundElement() {
+    public void whenLinearSearchAndNotFoundElement() {
         Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int result = ParallelIndexFinder.findIndex(array, 12);
+        int result = new ParallelIndexFinder<Integer>().findIndex(array, 12);
         assertThat(result, is(-1));
     }
 
     @Test
-    public void whenLinearSearchFoundElement() {
+    public void whenLinearSearchAndFoundElement() {
         Integer[] array = {1, 5, 8, 6, 3, 10, 11, 2, 9};
-        int result = ParallelIndexFinder.findIndex(array, 9);
+        int result = new ParallelIndexFinder<Integer>().findIndex(array, 9);
         assertThat(result, is(8));
     }
 
     @Test
-    public void whenForkJoinSearchFoundElement() {
+    public void whenForkJoinSearchAndFoundElement() {
         Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        int result = ParallelIndexFinder.findIndex(array, 9);
+        int result = new ParallelIndexFinder<Integer>().findIndex(array, 9);
         assertThat(result, is(8));
     }
 
     @Test
-    public void whenForkJoinSearchNotFoundElement() {
+    public void whenForkJoinSearchAndNotFoundElement() {
         Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        int result = ParallelIndexFinder.findIndex(array, 15);
+        int result = new ParallelIndexFinder<Integer>().findIndex(array, 15);
         assertThat(result, is(-1));
+    }
+
+    @Test
+    public void whenForkJoinSearchCharAndFoundElement() {
+        Character[] array = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'};
+        int result = new ParallelIndexFinder<Character>().findIndex(array, 'c');
+        assertThat(result, is(2));
     }
 }
